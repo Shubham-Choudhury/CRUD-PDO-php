e71c1718c6946bcc0f528b3222046c88dbb9c96c
Shubham Choudhury <shubhamchoudhury2002@yahoo.com>
2022-09-30 19:36:55 +0530

    Some Changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>CRUD PDO php</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
@@ -48,7 +48,7 @@
                             <td><?php echo $user->email; ?></td>
                             <td><?php echo $user->address; ?></td>
                             <td>
-                                <a href="edit.php?id=<?php echo $user->id; ?>" class="btn btn-warning">Edit</a>
+                                <a href="edit.php?id=<?php echo htmlspecialchars($user->id); ?>" class="btn btn-warning">Edit</a>
                                 <a href="delete.php?id=<?php echo $user->id; ?>" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
